1995dec9847c6ba8d948c7db4c760d10e6ba95e3
ethansheridansmith <ethansheridansmith@gmail.com>
2023-09-25 22:29:36 +0100

    taking a break saving for now

---
+++ react-app/src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import "../styles/NavBar.css";
+
+function NavBar() {
+  return (
+    <div className='navbar'>
+        <div className='toggleButton'>
+            <button></button>
+        </div>
+        <div className='links'>
+            <Link to="/"> Home </Link>
+            <Link to="/projects"> Projects </Link>
+            <Link to="/experience"> Experience </Link>
+            <Link to="/education"> Education </Link>
+        </div>
+    </div>
+  );
+}
+
+export default NavBar
